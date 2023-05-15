208eb6538a2ed3ec155ac3702fd05e886da81849
Diego Fernández M. (dfmontesinos) <107031333+diegofernandezmontesinos@users.noreply.github.com>
2023-05-15 18:17:59 +0200

    creacion del proyecto
    creacion del proyecto

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+
+ReactDOM.render(
+    <div> My Wish List from REACT, created by DFM</div>,
+    document.getElementById("root")
+);
