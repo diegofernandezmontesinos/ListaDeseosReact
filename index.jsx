5cad221de3932a2c944fec5dc8cf31c1ac67fe65
dfmontesinos <dfmontesinos@gmail.com>
2023-05-16 16:36:19 +0200

    cambios front

--- index.jsx
+++ index.jsx
@@ -3,6 +3,6 @@ import  ReactDOM  from "react-dom";
 
 
 ReactDOM.render(
-    <div> lista deseos con react</div>,
+    <div> lista deseos con react, creada por mi</div>,
     document.getElementById("root")
 );
